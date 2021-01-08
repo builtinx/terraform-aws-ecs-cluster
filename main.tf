2a4ff11e74ef874eba606a7d558b4670d5befd3b
Sam Chen <sam.chen@builtin.com>
2021-01-08 08:18:05 -0600

    feat(SRE-378): copy asg

--- main.tf
+++ main.tf
@@ -211,6 +211,64 @@ resource "aws_autoscaling_group" "container_instance" {
   }
 }
 
+resource "aws_launch_configuration" "container_instance" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  root_block_device {
+    volume_type = "${var.root_block_device_type}"
+    volume_size = "${var.root_block_device_size}"
+  }
+
+  name_prefix          = "lc${title(var.environment)}ContainerInstance-"
+  iam_instance_profile = "${aws_iam_instance_profile.container_instance.name}"
+
+  # Using join() is a workaround for depending on conditional resources.
+  # https://github.com/hashicorp/terraform/issues/2831#issuecomment-298751019
+  image_id = "${var.lookup_latest_ami ? join("", data.aws_ami.ecs_ami.*.image_id) : join("", data.aws_ami.user_ami.*.image_id)}"
+
+  instance_type   = "${var.instance_type}"
+  key_name        = "${var.key_name}"
+  security_groups = ["${aws_security_group.container_instance.id}"]
+  user_data       = "${data.template_cloudinit_config.container_instance_cloud_config.rendered}"
+}
+
+resource "aws_autoscaling_group" "container_instance" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  name                      = "asg${title(var.environment)}ContainerInstance"
+  launch_configuration      = "${aws_launch_configuration.container_instance.name}"
+  health_check_grace_period = "${var.health_check_grace_period}"
+  health_check_type         = "EC2"
+  desired_capacity          = "${var.desired_capacity}"
+  termination_policies      = ["OldestLaunchConfiguration", "Default"]
+  min_size                  = "${var.min_size}"
+  max_size                  = "${var.max_size}"
+  enabled_metrics           = ["${var.enabled_metrics}"]
+  vpc_zone_identifier       = ["${var.private_subnet_ids}"]
+
+  tag {
+    key                 = "Name"
+    value               = "ContainerInstance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Project"
+    value               = "${var.project}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = "${var.environment}"
+    propagate_at_launch = true
+  }
+}
+
 #
 # ECS resources
 #
